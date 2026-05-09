d33c2115503c216e6f7d74f9b29b99db7a5978b4
fapelicioli <fapelicioli@ucs.br>
2026-05-09 14:41:47 -0300

    Update User.java
    Definição base de usuário

--- src/br/ucs/poo/tb1/user/User.java
+++ src/br/ucs/poo/tb1/user/User.java
@@ -1,6 +1,32 @@
 package br.ucs.poo.tb1.user;
 
-
 public class User {
+	
+	int id = 0;
+	String nome = "user";
+	String senha = "default";
+	final int permlevel = 0;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public int getPermlevel() {
+		return permlevel;
+	}
 	
 }
