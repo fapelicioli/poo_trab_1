ae6594a77b47253ae1df0afb20c5a37cb33f89b4
fapelicioli <fapelicioli@ucs.br>
2026-05-09 14:50:43 -0300

    Update Cadastro.java
    Alteração para classe abstrata e base criada

--- src/br/ucs/poo/tb1/cadastro/Cadastro.java
+++ src/br/ucs/poo/tb1/cadastro/Cadastro.java
@@ -1,5 +1,21 @@
 package br.ucs.poo.tb1.cadastro;
 
-public interface Cadastro {
-
+public abstract class Cadastro {
+	
+	int id;
+	String nome;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
