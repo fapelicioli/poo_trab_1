4529e4805625ac654ac118be684df89215b902ae
fapelicioli <fapelicioli@ucs.br>
2026-05-09 16:10:51 -0300

    Update Transportadora.java

--- src/br/ucs/poo/tb1/cadastro/Transportadora.java
+++ src/br/ucs/poo/tb1/cadastro/Transportadora.java
@@ -1,5 +1,11 @@
 package br.ucs.poo.tb1.cadastro;
 
+import java.util.ArrayList;
+
 public class Transportadora extends Fornecedor{
 
+	public Transportadora(int id, String nome, String cnpj, ArrayList<Produto> produtos) {
+		super(id, nome, cnpj, produtos);
+	}
+	
 }
