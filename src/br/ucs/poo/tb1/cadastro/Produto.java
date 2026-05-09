c24782d1fdd1616ca8a51ad63b7b8a33a8aee3f4
fapelicioli <fapelicioli@ucs.br>
2026-05-09 16:10:41 -0300

    Update Produto.java

--- src/br/ucs/poo/tb1/cadastro/Produto.java
+++ src/br/ucs/poo/tb1/cadastro/Produto.java
@@ -1,5 +1,29 @@
 package br.ucs.poo.tb1.cadastro;
 
-public class Produto implements Cadastro{
-
+public class Produto extends Cadastro{
+	
+	String sku;
+	int idFornecedor;
+	
+	public Produto(int id, String nome, String sku, int idFornecedor) {
+		this.id = id;
+		this.nome = nome;
+		this.sku = sku;
+		this.idFornecedor = idFornecedor;
+	}
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public int getIdFornecedor() {
+		return idFornecedor;
+	}
+	public void setIdFornecedor(int idFornecedor) {
+		this.idFornecedor = idFornecedor;
+	}
+	
+	
 }
