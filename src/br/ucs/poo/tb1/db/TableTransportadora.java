a323f31d464014d7dc855fb636a1cfc24cf8b4bf
fapelicioli <fapelicioli@ucs.br>
2026-05-10 02:33:14 -0300

    Update TableTransportadora.java

--- src/br/ucs/poo/tb1/db/TableTransportadora.java
+++ src/br/ucs/poo/tb1/db/TableTransportadora.java
@@ -1,5 +1,38 @@
 package br.ucs.poo.tb1.db;
 
+import java.util.Map;
+import java.util.TreeMap;
+import br.ucs.poo.tb1.cadastro.Transportadora;
+
 public class TableTransportadora{
+
+	Map<Integer, Transportadora> data = new TreeMap<>();
+	
+	public void incluir(int id, Transportadora transportadora) {
+		data.put(id, transportadora);
+	}
+
+	public void alterar(int id, Transportadora transportadora) {
+		data.remove(id);
+		data.put(id, transportadora);
+	}
+
+	public void excluir(int id) {
+		data.remove(id);
+	}
+
+	public Transportadora consulta(int id) {
+		return data.get(id);
+	}
+	
+	public Transportadora consulta(String nome) {
+		for(Transportadora i : data.values()) {
+			String temp = i.getNome();
+			if(temp == nome) {
+				return i;
+			}
+		}
+		return null;
+	}
 
 }
