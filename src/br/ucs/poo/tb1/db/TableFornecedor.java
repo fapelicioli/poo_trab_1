e94ccaeec314e533d138fbd0d2a7c5fce0396afa
fapelicioli <fapelicioli@ucs.br>
2026-05-10 02:32:39 -0300

    Update TableFornecedor.java

--- src/br/ucs/poo/tb1/db/TableFornecedor.java
+++ src/br/ucs/poo/tb1/db/TableFornecedor.java
@@ -1,5 +1,38 @@
 package br.ucs.poo.tb1.db;
 
+import java.util.Map;
+import java.util.TreeMap;
+import br.ucs.poo.tb1.cadastro.Fornecedor;
+
 public class TableFornecedor{
+	
+	Map<Integer, Fornecedor> data = new TreeMap<>();
+	
+	public void incluir(int id, Fornecedor fornecedor) {
+		data.put(id, fornecedor);
+	}
+
+	public void alterar(int id, Fornecedor fornecedor) {
+		data.remove(id);
+		data.put(id, fornecedor);
+	}
+
+	public void excluir(int id) {
+		data.remove(id);
+	}
+
+	public Fornecedor consulta(int id) {
+		return data.get(id);
+	}
+	
+	public Fornecedor consulta(String nome) {
+		for(Fornecedor i : data.values()) {
+			String temp = i.getNome();
+			if(temp == nome) {
+				return i;
+			}
+		}
+		return null;
+	}
 
 }
