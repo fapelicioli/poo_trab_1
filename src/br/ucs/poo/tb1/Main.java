0ea9bd899f20ed8e68b91ce923775b303120e852
fapelicioli <fapelicioli@ucs.br>
2026-05-11 00:04:11 -0300

    Update Main.java

--- src/br/ucs/poo/tb1/Main.java
+++ src/br/ucs/poo/tb1/Main.java
@@ -18,9 +18,22 @@ public class Main {
 		//Inclui dados base na db
 		Admin admbase = new Admin("admin", "admin");
 		User userbase = new User("user","user");
+		Fornecedor fornecedor1 = new Fornecedor("azul","12345");
+		Fornecedor fornecedor2 = new Fornecedor("rosa","12345");
+		Produto produto1 = new Produto("Porta","P001.1",1);
+		Produto produto2 = new Produto("Porta verde","P001.2",1);
+		
 		
 		tabelau.incluir(admbase);
 		tabelau.incluir(userbase);
+		
+		tabelaf.incluir(fornecedor1);
+		tabelaf.incluir(fornecedor2);
+		
+		tabelap.incluir(produto1);
+		tabelaf.consulta(1).addProduto(produto1);
+		tabelap.incluir(produto2);
+		tabelaf.consulta(1).addProduto(produto2);
 		
 		//menu.login(tabelau);
 		
