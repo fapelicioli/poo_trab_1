d2c4aa5b065ac482cee94786e57d31e5d05841e1
fapelicioli <fapelicioli@ucs.br>
2026-05-11 14:42:08 -0300

    Add Carga

---
+++ src/br/ucs/poo/tb1/cadastro/Carga.java
@@ -0,0 +1,32 @@
+package br.ucs.poo.tb1.cadastro;
+
+import java.util.ArrayList;
+
+public class Carga{
+	int id;
+	ArrayList<Produto> produtos = new ArrayList<Produto>();
+	
+	public Carga(ArrayList<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArrayList<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(ArrayList<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	public void addProduto(Produto produto) {
+		this.produtos.add(produto);
+	}
+	public void removeProduto(Produto produto) {
+		this.produtos.remove(produto);
+	}
+	
+}
