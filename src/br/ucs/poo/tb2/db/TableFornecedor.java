957e8914a5f8fe0a4c89ed3b73c1369c0d1e7bdf
fapelicioli <fapelicioli@ucs.br>
2026-06-22 21:02:41 -0300

    Update TableFornecedor.java
    Implementacao base de arquivo

--- src/br/ucs/poo/tb2/db/TableFornecedor.java
+++ src/br/ucs/poo/tb2/db/TableFornecedor.java
@@ -1,7 +1,14 @@
 package br.ucs.poo.tb2.db;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
 
 import br.ucs.poo.tb2.cadastro.Fornecedor;
 
@@ -9,8 +16,18 @@ public class TableFornecedor{
 	
 	private Map<Integer, Fornecedor> data;
 	private int counter;
+	File database;
 	
-	public TableFornecedor() {
+	public TableFornecedor(File diretorio) {
+		
+		try {
+			database = new File(diretorio.getPath()+"/data/tablefornecedor.dat");
+			database.createNewFile();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		data = new TreeMap<>();
 		counter = 1;
 	}
@@ -19,6 +36,17 @@ public class TableFornecedor{
 		int id = counter;
 		fornecedor.setId(id);
 		data.put(id, fornecedor);
+		try {
+			FileOutputStream dataOut = new FileOutputStream(database.getPath());
+			ObjectOutputStream objectOut = new ObjectOutputStream(dataOut);
+			objectOut.writeObject(fornecedor);
+			objectOut.close();
+			dataOut.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		counter++;
 		return id;
 	}
