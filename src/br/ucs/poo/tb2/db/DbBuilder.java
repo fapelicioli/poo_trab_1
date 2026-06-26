ff027da9e28d46bf033bfd26a2368b6dd39d3b43
fapelicioli <fapelicioli@ucs.br>
2026-06-26 16:11:16 -0300

    Update DbBuilder.java

--- src/br/ucs/poo/tb2/db/DbBuilder.java
+++ src/br/ucs/poo/tb2/db/DbBuilder.java
@@ -21,7 +21,7 @@ public class DbBuilder {
 	public void inicializa() {
 		
 		Admin admbase = new Admin("admin", "admin");
-		User userbase = new User("user","user","Interno");
+		User userbase = new User("user","user");
 		
 		tabelau.incluir(admbase);
 		tabelau.incluir(userbase);
@@ -29,75 +29,75 @@ public class DbBuilder {
 		
 		Fornecedor fornecedor = new Fornecedor("P & J LTDA","30.973.354/0001-83");
 		tabelaf.incluir(fornecedor);
-		Produto produto = new Produto("Porta madeirada","P001.1",1);
+		Produto produto = new Produto("Porta madeirada","P001.1",1,250.00f,10);
 		tabelap.incluir(produto);
 		tabelaf.consulta(1).addProduto(produto);
-		produto = new Produto("Porta preta","P001.2",1);
+		produto = new Produto("Porta preta","P001.2",1,250.00f,15);
 		tabelap.incluir(produto);
 		tabelaf.consulta(1).addProduto(produto);
-		produto = new Produto("Porta branca","P001.3",1);
+		produto = new Produto("Porta branca","P001.3",1,250.00f,5);
 		tabelap.incluir(produto);
 		tabelaf.consulta(1).addProduto(produto);
-		produto = new Produto("Janela vitral transparente","J001.1",1);
+		produto = new Produto("Janela vitral transparente","J001.1",1,150.00f,15);
 		tabelap.incluir(produto);
 		tabelaf.consulta(1).addProduto(produto);
-		produto = new Produto("Janela vitral colorida","J001.2",1);
+		produto = new Produto("Janela vitral colorida","J001.2",1,150.00f,5);
 		tabelap.incluir(produto);
 		tabelaf.consulta(1).addProduto(produto);
-		produto = new Produto("Janela madeirada ipe","J002.1",1);
+		produto = new Produto("Janela madeirada ipe","J002.1",1,150.00f,12);
 		tabelap.incluir(produto);
 		tabelaf.consulta(1).addProduto(produto);
-		produto = new Produto("Janela madeirada acacia","J002.2",1);
+		produto = new Produto("Janela madeirada acacia","J002.2",1,150.00f,4);
 		tabelap.incluir(produto);
 		tabelaf.consulta(1).addProduto(produto);
 		
 		
 		fornecedor = new Fornecedor("Cica-Cola Refrigerantes & Bebidas","84.157.771/0001-87");
 		tabelaf.incluir(fornecedor);
-		produto = new Produto("Refrigerante de cola","R100.1",2);
+		produto = new Produto("Refrigerante de cola","R100.1",2,2.50f,20);
 		tabelap.incluir(produto);
 		tabelaf.consulta(2).addProduto(produto);
-		produto = new Produto("Refrigerante de laranja","R100.2",2);
+		produto = new Produto("Refrigerante de laranja","R100.2",2,2.50f,25);
 		tabelap.incluir(produto);
 		tabelaf.consulta(2).addProduto(produto);
-		produto = new Produto("Refrigerante de cereja","R100.5",2);
+		produto = new Produto("Refrigerante de cereja","R100.5",2,3.00f,30);
 		tabelap.incluir(produto);
 		tabelaf.consulta(2).addProduto(produto);
-		produto = new Produto("Agua mineral sem gas","A101.1",2);
+		produto = new Produto("Agua mineral sem gas","A101.1",2,1.50f,50);
 		tabelap.incluir(produto);
 		tabelaf.consulta(2).addProduto(produto);
-		produto = new Produto("Agua mineral com gas","A101.2",2);
+		produto = new Produto("Agua mineral com gas","A101.2",2,1.50f,50);
 		tabelap.incluir(produto);
 		tabelaf.consulta(2).addProduto(produto);
-		produto = new Produto("Suco de uva","S256.1",2);
+		produto = new Produto("Suco de uva","S256.1",2,3.00f,20);
 		tabelap.incluir(produto);
 		tabelaf.consulta(2).addProduto(produto);
-		produto = new Produto("Agua tonica","A113.1",2);
+		produto = new Produto("Agua tonica","A113.1",2,2.00f,40);
 		tabelap.incluir(produto);
 		tabelaf.consulta(2).addProduto(produto);
 		
 		
 		fornecedor = new Fornecedor("SaborFood Salgados & Doces","57.415.130/0001-02");
 		tabelaf.incluir(fornecedor);
-		produto = new Produto("Coxinha congelada","C4968.1",3);
+		produto = new Produto("Coxinha congelada","C4968.1",3,45.50f,5);
 		tabelap.incluir(produto);
 		tabelaf.consulta(3).addProduto(produto);
-		produto = new Produto("Pao preto caseiro","Pc101.1",3);
+		produto = new Produto("Pao preto caseiro","Pc101.1",3,8.00f,3);
 		tabelap.incluir(produto);
 		tabelaf.consulta(3).addProduto(produto);
-		produto = new Produto("Cereal de frutas","Ce142.1",3);
+		produto = new Produto("Cereal de frutas","Ce142.1",3,20.25f,2);
 		tabelap.incluir(produto);
 		tabelaf.consulta(3).addProduto(produto);
-		produto = new Produto("Pao de forma caseiro","Pc100.1",3);
+		produto = new Produto("Pao de forma caseiro","Pc100.1",3,8.00f,5);
 		tabelap.incluir(produto);
 		tabelaf.consulta(3).addProduto(produto);
-		produto = new Produto("Lasanha congelada de frango","C4985.1",3);
+		produto = new Produto("Lasanha congelada de frango","C4985.1",3,40.99f,10);
 		tabelap.incluir(produto);
 		tabelaf.consulta(3).addProduto(produto);
-		produto = new Produto("Lasanha congelada 4 queijos","C4985.4",3);
+		produto = new Produto("Lasanha congelada 4 queijos","C4985.4",3,40.99f,15);
 		tabelap.incluir(produto);
 		tabelaf.consulta(3).addProduto(produto);
-		produto = new Produto("Sorvete de limao","S2690.8",3);
+		produto = new Produto("Sorvete de limao","S2690.8",3,18.50f,25);
 		tabelap.incluir(produto);
 		tabelaf.consulta(3).addProduto(produto);
 		
