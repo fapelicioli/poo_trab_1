76ff8a8d0bddb18c49857975e95920d554659c5b
fapelicioli <fapelicioli@ucs.br>
2026-06-26 11:28:54 -0300

    Update TableTransportadora.java

--- src/br/ucs/poo/tb2/db/TableTransportadora.java
+++ src/br/ucs/poo/tb2/db/TableTransportadora.java
@@ -1,30 +1,40 @@
 package br.ucs.poo.tb2.db;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Map;
 import java.util.TreeMap;
 
 import br.ucs.poo.tb2.cadastro.Transportadora;
 
-public class TableTransportadora{
+public class TableTransportadora implements Serializable{
 
 	private Map<Integer, Transportadora> data;
 	private int counter;
 	File database;
 	
 	public TableTransportadora(File diretorio) {
-		
+		database = new File(diretorio.getPath()+"/data/tabletransportadora.dat");
 		try {
-			database = new File(diretorio.getPath()+"/data/tabletransportadora.dat");
 			database.createNewFile();
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		data = new TreeMap<>();
 		counter = 1;
+	}
+	
+	public void salvar() {
+		try {
+			FileOutputStream dataOut = new FileOutputStream(database.getPath());
+			ObjectOutputStream objectOut = new ObjectOutputStream(dataOut);
+			objectOut.writeObject(this);
+			objectOut.close();
+			dataOut.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public int incluir(Transportadora transportadora) {
@@ -32,16 +42,19 @@ public class TableTransportadora{
 		transportadora.setId(id);
 		data.put(id, transportadora);
 		counter++;
+		salvar();
 		return id;
 	}
 
 	public void alterar(int id, Transportadora transportadora) {
 		data.remove(id);
 		data.put(id, transportadora);
+		salvar();
 	}
 
 	public void excluir(int id) {
 		data.remove(id);
+		salvar();
 	}
 
 	public Transportadora consulta(int id) {
