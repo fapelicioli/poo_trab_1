a000a64ee0a4136f89eaafb07b68363168ad5ce7
fapelicioli <fapelicioli@ucs.br>
2026-06-26 16:10:55 -0300

    Update Produto.java

--- src/br/ucs/poo/tb2/cadastro/Produto.java
+++ src/br/ucs/poo/tb2/cadastro/Produto.java
@@ -1,15 +1,27 @@
 package br.ucs.poo.tb2.cadastro;
 
+import br.ucs.poo.tb2.excecoes.*;
+
 public class Produto extends Cadastro{
 	
 	private String sku;
 	private int idFornecedor;
+	private int quantidade;
+	private float valor;
 	
-	public Produto(String nome, String sku, int idFornecedor) {
+	public Produto(String nome, String sku, int idFornecedor, float valor) {
 		this.id = 0;
 		this.nome = nome;
 		this.sku = sku;
 		this.idFornecedor = idFornecedor;
+		this.quantidade = 0;
+	}
+	public Produto(String nome, String sku, int idFornecedor,float valor, int quant) {
+		this.id = 0;
+		this.nome = nome;
+		this.sku = sku;
+		this.idFornecedor = idFornecedor;
+		this.quantidade = quant;
 	}
 	public String getSku() {
 		return sku;
@@ -23,6 +35,24 @@ public class Produto extends Cadastro{
 	public void setIdFornecedor(int idFornecedor) {
 		this.idFornecedor = idFornecedor;
 	}
-	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public void removeEstoque(int quantidade)throws StockException{
+		if(quantidade > this.quantidade) {
+			throw new StockException("\nQuantidade insuficiente em estoque, estoque atual: " + this.quantidade);
+		} else {
+			this.quantidade -= quantidade;
+		}
+	}
 	
 }
