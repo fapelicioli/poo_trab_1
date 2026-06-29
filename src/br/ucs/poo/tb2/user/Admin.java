6e85feea9921a53cd51815e34eed468c4269f7c3
fapelicioli <fapelicioli@ucs.br>
2026-06-29 16:32:00 -0300

    Update Admin.java

--- src/br/ucs/poo/tb2/user/Admin.java
+++ src/br/ucs/poo/tb2/user/Admin.java
@@ -1,9 +1,9 @@
 package br.ucs.poo.tb2.user;
 
 public class Admin extends User{
-
+	
 	public Admin(String nome, String senha) {
-		super(nome, senha, "Interno");
+		super(nome, senha);
 		super.permlevel = 1;
 	}
 	
